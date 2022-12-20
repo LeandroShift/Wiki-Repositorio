fbc22dceda16a0493e1b8ad645777150df0664e5
LeandroShift <leandromendesluiz@hotmail.com>
2022-12-19 23:48:14 -0300

    Criando Uma Wiki - Desafio Dio

---
+++ src/components/Input/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+
+export const InputContainer = styled.div`
+ 
+    border: 1px solid #FAFAFA;
+    border-radius: 28px;
+
+    height: 62px;
+    width: 80%;
+
+    margin: 20px;
+
+    input{
+        background: transparent;
+        border: 0 20px;
+        width: 94%;
+        heignt: 62px;
+        padding: 20px 20px;
+        color : #FFFFFF;
+        font-size: 20px;
+    }
+
+
+
+`
